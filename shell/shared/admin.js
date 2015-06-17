ed2f39ae02bb60cb6e796b42759cddcd5fa95cea
Jason Paryani <github@jparyani.com>
2015-06-17 15:08:16 -0700

    Move IpNetwork/IpInterface offers to their own tab

--- shell/shared/admin.js
+++ shell/shared/admin.js
@@ -72,6 +72,10 @@ var adminInvitesRoute = adminRoute.extend({
   settingsTab: "adminInvites"
 });
 
+var adminCapsRoute = adminRoute.extend({
+  settingsTab: "adminCaps"
+});
+
 Router.map(function () {
   this.route("admin", {
     path: "/admin/settings/:_token?",
@@ -93,6 +97,10 @@ Router.map(function () {
     path: "/admin/invites/:_token?",
     controller: adminInvitesRoute
   });
+  this.route("adminCaps", {
+    path: "/admin/capabilities/:_token?",
+    controller: adminCapsRoute
+  });
   this.route("adminOld", {
     path: "/admin/:_token?",
     controller: adminRoute
@@ -112,6 +120,7 @@ if (Meteor.isClient) {
     state.set("errors", []);
     state.set("fadeAlert", false);
     state.set("successMessage", "Your settings have been saved.");
+    state.set("powerboxOfferUrl", null);
   };
 
   Meteor.startup(function () {
@@ -173,6 +182,9 @@ if (Meteor.isClient) {
     logActive: function () {
       return Iron.controller().state.get("settingsTab") == "adminLog";
     },
+    capsActive: function () {
+      return Iron.controller().state.get("settingsTab") == "adminCaps";
+    },
     getToken: getToken
   });
 
@@ -226,24 +238,6 @@ if (Meteor.isClient) {
       state.set("successMessage", "Email has been sent.");
       Meteor.call("testSend", this.token, document.getElementById("smptUrl").value,
                   document.getElementById("email-test-to").value, handleErrorBound);
-      return false; // prevent form from submitting
-    },
-    "click #offer-ipnetwork": function (event) {
-      var state = Iron.controller().state;
-      resetResult(state);
-      Meteor.call("offerIpNetwork", this.token, function (err, webkey) {
-        state.set("successMessage", "IpNetwork webkey created: " + webkey);
-        handleError.call(state, err);
-      });
-      return false; // prevent form from submitting
-    },
-    "click #offer-ipinterface": function (event) {
-      var state = Iron.controller().state;
-      resetResult(state);
-      Meteor.call("offerIpInterface", this.token, function (err, webkey) {
-        state.set("successMessage", "IpInterface webkey created: " + webkey);
-        handleError.call(state, err);
-      });
       return false; // prevent form from submitting
     },
     "submit #admin-settings-form": function (event) {
@@ -604,6 +598,40 @@ if (Meteor.isClient) {
       return AnsiUp.ansi_to_html(AdminLog.find({}, {$sort: {_id: 1}})
               .map(function (entry) { return entry.text; })
               .join(""), {use_classes:true});
+    }
+  });
+
+  Template.adminCaps.helpers({
+    powerboxOfferUrl: function () {
+      var state = Iron.controller().state;
+      return state.get("powerboxOfferUrl");
+    }
+  })
+
+  Template.adminCaps.events({
+    "click #offer-ipnetwork": function (event) {
+      var state = Iron.controller().state;
+      resetResult(state);
+      state.set("successMessage", "IpNetwork webkey created. See powerbox UI for your webkey.");
+      Meteor.call("offerIpNetwork", this.token, function (err, webkey) {
+        state.set("powerboxOfferUrl", webkey);
+        handleError.call(state, err);
+      });
+      return false; // prevent form from submitting
+    },
+    "click #offer-ipinterface": function (event) {
+      var state = Iron.controller().state;
+      resetResult(state);
+      state.set("successMessage", "IpInterface webkey created. See powerbox UI for your webkey.");
+      Meteor.call("offerIpInterface", this.token, function (err, webkey) {
+        state.set("powerboxOfferUrl", webkey);
+        handleError.call(state, err);
+      });
+      return false; // prevent form from submitting
+    },
+    "click #powerbox-offer-popup-closer": function (event) {
+      var state = Iron.controller().state;
+      return state.set("powerboxOfferUrl", null);
     }
   });
 }
