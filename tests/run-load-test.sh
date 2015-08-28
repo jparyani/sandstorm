d3b5cb63882bee4f41dffd33570161a58f5473ea
Jason Paryani <github@jparyani.com>
2015-08-28 14:44:51 -0700

    add script

---
+++ tests/run-load-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Sandstorm - Personal Cloud Sandbox
+# Copyright (c) 2014 Sandstorm Development Group, Inc. and contributors
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+export PATH=/bin:/usr/bin:$PATH
+xvfb-run ./node_modules/selenium-standalone/bin/selenium-standalone start &
+sleep 30
+./node_modules/.bin/nightwatch >> /var/log/sandstorm-test.log
