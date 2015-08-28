e4bec49597a6b5040e43e6ef02b7c4fd54645d67
Jason Paryani <github@jparyani.com>
2015-08-28 14:30:58 -0700

    Load testing

---
+++ tests/load_test/grain.js
@@ -0,0 +1,68 @@
+// Sandstorm - Personal Cloud Sandbox
+// Copyright (c) 2014 Sandstorm Development Group, Inc. and contributors
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+var utils = require('../utils'),
+    short_wait = utils.short_wait,
+    medium_wait = utils.medium_wait,
+    long_wait = utils.long_wait,
+    very_long_wait = utils.very_long_wait;
+var path = require('path');
+var assetsPath = path.resolve(__dirname, '../assets');
+
+module.exports = {};
+
+
+function install (browser) {
+  browser
+    .loginDemo()
+    .url(browser.launch_url + "/install/ca690ad886bf920026f8b876c19539c1?url=http://sandstorm.io/apps/ssjekyll8.spk")
+    .waitForElementVisible('#step-confirm', very_long_wait)
+    .click('#confirmInstall')
+    .waitForElementVisible('.app-action[data-app-id="nqmcqs9spcdpmqyuxemf0tsgwn8awfvswc58wgk375g4u25xv6yh"]', short_wait)
+    .assert.containsText('.app-action[data-app-id="nqmcqs9spcdpmqyuxemf0tsgwn8awfvswc58wgk375g4u25xv6yh"]>.app-title', 'Hacker CMS');
+}
+
+function testNew (browser) {
+  browser
+    .url(browser.launch_url + "/grain/new")
+    .waitForElementVisible('.app-list', medium_wait)
+    .waitForElementVisible('.app-action[data-app-id="nqmcqs9spcdpmqyuxemf0tsgwn8awfvswc58wgk375g4u25xv6yh"]', short_wait)
+    .click('.app-action[data-app-id="nqmcqs9spcdpmqyuxemf0tsgwn8awfvswc58wgk375g4u25xv6yh"]')
+    .waitForElementVisible('#grainTitle', medium_wait)
+    .assert.containsText('#grainTitle', 'Untitled Hacker CMS Site');
+}
+
+function testGrain (browser) {
+  browser
+    .pause(short_wait)
+    .frame('grain-frame')
+    .waitForElementPresent('#publish', medium_wait)
+    .assert.containsText('#publish', 'Publish')
+    .frame(null)
+    .waitForElementVisible(".delete", short_wait)
+    .click(".delete");
+};
+// https://testrock.sandstorm.io/install/f0c18bb97e5f0aa76dc2e709a3b0b449?url=http://ethercalc.net/ethercalc-201505183.spk
+for (var i = 0; i < 20000; i++) {
+  module.exports["Test install " + i] = install;
+
+  for (var j = 0; j < 5; j++) {
+    module.exports["Test new grain " + i + " " + j] = testNew;
+    module.exports["Test grain frame " + i + " " + j] = testGrain;
+  }
+}
